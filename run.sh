64229fa966a9b235bcc663717c2984b160f27186
pierewoj <kuba.pierewoj@gmail.com>
2017-04-09 01:05:31 +0200

    fix literowki

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker-compose -f docker-compose-infra.yml up -d
-docker exec -ti bigos_kisiel_1 redis-cli sadd to_crawl http://kwestiasmaku.command
+docker exec -ti bigos_kisiel_1 redis-cli sadd to_crawl http://kwestiasmaku.com
 docker-compose up -d
